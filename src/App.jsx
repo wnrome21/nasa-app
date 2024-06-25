5908e4ed2e3ecfc3d6decfc461acdc13f9c53264
nater <n.rome26@gmail.com>
2024-06-25 14:41:49 -0700

    added local storage to fetch api from the cache if api has already been loaded that day

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,24 @@ function App() {
     async function fetchAPIData() {
       const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
       const url = `https://api.nasa.gov/planetary/apod?api_key=${NASA_KEY}`;
+
+      const today = (new Date()). toDateString()
+      const localKey = `NASA-${today}}`
+      if (localStorage.getItem(localKey)) {
+        const apiData = JSON.parse(localStorage.getItem(localKey))
+        setData(apiData)
+        console.log("Fetched from cache today")
+        return
+      }
+      localStorage.clear()
+
       try {
         const res = await fetch(url);
         const apiData = await res.json();
+        localStorage.setItem(localKey, JSON.stringify(apiData))
+        
         setData(apiData);
-        console.log("DATA\n", apiData);
+        console.log("Fetched from API today.")
       } catch (err) {
         console.log(err.message);
       }
